409be8fff9e45a0f8ad7585ae9781f1bf8f1c752
student <student@D40>
2017-12-07 20:17:14 +0200

    loops

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -10,6 +10,93 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
+            //int a = 0;
+            //do
+            //{
+            //    Console.WriteLine(a);
+            //    a++;               
+            //} while (a < 10);
+
+            //int i = 1;
+            //Console.WriteLine($"{i++ + i++ + ++i + ++i + ++i}");
+
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    Console.WriteLine("Message.");                            
+            //}
+
+            ////5.1
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.Write("a");
+            //}
+
+            ////5.1
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.WriteLine("a");
+            //}
+
+            ////5.2
+            //for (int i = 0; i <= 255; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            ////5.2
+            //for (int i = 255; i <= 0; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            ////5.3
+            //int a, b;
+            //Console.Write("Введите первое число: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //b = int.Parse(Console.ReadLine());
+            //for(int i = a; i <=b; i++)
+            //{
+            //    if(i%2==0)
+            //        Console.WriteLine(i);
+            //}
+
+            //5.4
+            //for (int i = 40; i <= 255; i++)
+            //{
+            //    if (i % 10 == 0)
+            //        Console.WriteLine();
+
+            //    Console.Write($"{i} - ");
+            //    Console.Write((char)i);
+            //    Console.Write("| ");
+            //}
+
+            //5.5
+            //int n, sum = 0;
+            //Console.Write("Введите число: ");
+            //n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //    sum += i;
+            //Console.WriteLine($"Сумма чисел от 1 до {n} = {sum}");
+
+            //5.6
+            //int n;
+            //Console.Write("Введите количество строк: ");
+            //n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //        Console.Write("*");
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for (int j = i - 1; j >= 1; j--)
+            //        Console.Write("*");
+            //    Console.WriteLine();
+            }
         }
     }
 }
